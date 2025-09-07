4876bfa6bb508d3b22c65e6f69e58a0f2335876b
obaidallah <obaidalqershi1@gmail.com>
2025-09-07 22:54:04 +0300

    implement the auth from amplify and sign in page

---
+++ client/src/app/(auth)/authProvider.tsx
@@ -0,0 +1,103 @@
+import React from 'react';
+import { Amplify } from 'aws-amplify';
+
+import { Authenticator, components, Heading, useAuthenticator, View } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
+
+
+
+Amplify.configure({
+    Auth:{
+        Cognito: {
+            userPoolId: process.env.NEXT_PUBLIC_AWS_COGNITO_POOL_ID!,
+            userPoolClientId: process.env.NEXT_PUBLIC_AWS_COGNITO_POOL_CLIENT_ID!
+        }
+    }
+});
+
+const components = {
+    Header(){
+        return (
+            <View className='mt-4 mb-7'>
+                <Heading level={3} className='!text-2xl !font-bold'>
+                    RENT
+                    <span className='text-secondary-500 font-light hover:!text-primary-300'>IFUL</span>
+                </Heading>
+                <p className='text-muted-foreground mt-2'>
+                    <span className='font-bold'>Welcome!</span> Please sign in to continue
+                </p>
+            </View>
+        )
+    },
+    SignIn:{
+        Footer(){
+            const {toSignUp} = useAuthenticator();
+            return (
+                <View className='text-center mt-4'>
+                    <p className='text-muted-foreground'>
+                        Dont&apos;t have an account?{" "}
+                    
+                    <button onClick={toSignUp} className='text-primary hover:underline bg-transparent border-none p-0'>
+                        Sign up here
+                    </button>
+                    </p>
+                </View>
+            )
+        }
+    }
+}
+
+const formFields = {
+    signIn:{
+        username:{
+            placeholder: "Enter your email",
+            label: "Email",
+            isRequired: true,
+        },
+        password:{
+            placeholder: "Enter your password",
+            label: "password",
+            isRequired: true,
+        },
+    },
+    signUp:{
+        username:{
+            order: 1,
+            placeholder: "Choose a username",
+            label: "Username",
+            isRequired: true,
+        },
+        email:{
+            order: 2,
+            placeholder: "Enter your email address",
+            label: "Email",
+            isRequired: true,
+        },
+        password:{
+            order: 3,
+            placeholder: "Create a password",
+            label: "Password",
+            isRequired: true,
+        },
+        confirm_password:{
+            order: 2,
+            placeholder: "Confirm your password",
+            label: "Confirm Password",
+            isRequired: true,
+        },
+    },
+}
+
+
+const Auth = ({children}: {children: React.ReactNode})=> {
+  return (
+    <div className='h-full'>
+    <Authenticator
+    components={components}
+    formFields={formFields}>
+    {()=><>{children}</>}
+    </Authenticator>
+    </div>
+  );
+}
+export default Auth
--- client/src/app/providers.tsx
+++ client/src/app/providers.tsx
@@ -1,11 +1,17 @@
 'use client'
 import React from "react"
 import StoreProvider from "@/state/redux"
+import {Authenticator} from '@aws-amplify/ui-react'
+import Auth from "./(auth)/authProvider"
 
 const Providers = ({children}: {children: React.ReactNode}) =>{
     return(
         <StoreProvider>
+            <Authenticator.Provider>
+                <Auth>
             {children}
+            </Auth>
+            </Authenticator.Provider>
         </StoreProvider>
     )
 }
