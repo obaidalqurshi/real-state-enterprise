9eb323f3f69dc5a7f9007c98e3a4b9cab12ff85f
obaidallah <obaidalqershi1@gmail.com>
2025-09-07 23:23:55 +0300

    auth ready

--- client/src/app/(auth)/authProvider.tsx
+++ client/src/app/(auth)/authProvider.tsx
@@ -1,8 +1,10 @@
-import React from 'react';
+'use client'
+import React, { useEffect } from 'react';
 import { Amplify } from 'aws-amplify';
 
 import { Authenticator, Heading, Radio, RadioGroupField, useAuthenticator, View } from '@aws-amplify/ui-react';
 import '@aws-amplify/ui-react/styles.css';
+import { useRouter, usePathname } from 'next/navigation';
 
 
 
@@ -124,9 +126,27 @@ const formFields = {
 
 
 const Auth = ({children}: {children: React.ReactNode})=> {
+    const {user} = useAuthenticator((context)=>[context.user])
+    const router = useRouter();
+    const pathname = usePathname();
+
+    const isAuthPage = pathname.match(/^\/(sigin|signup)$/);
+    const isDashboardPage = pathname.startsWith("/manager" ) || pathname.startsWith("/tenants");
+
+    useEffect(()=>{
+        if(user && isAuthPage){
+            router.push("/")
+        }
+    },[user, isAuthPage, router]);
+
+    if(!isAuthPage && !isDashboardPage){
+        return <>{children}</>
+    }
+
   return (
     <div className='h-full'>
     <Authenticator
+    initialState={pathname.includes('signup') ? 'signUp' : 'signIn'}
     components={components}
     formFields={formFields}>
     {()=><>{children}</>}
